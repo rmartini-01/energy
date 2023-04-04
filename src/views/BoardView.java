848add510e84cb42a1e95eefda6f26e57a0caa78
rmartini <reen.martini7@etu.univ-lorraine.fr>
2023-04-04 11:31:05 +0200

    affichage avec JPanel

--- src/views/BoardView.java
+++ src/views/BoardView.java
@@ -6,17 +6,16 @@ import models.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class BoardView extends JPanel {
     private JFrame frame;
     private final int level;
     private Board board;
-    public BoardView(JFrame frame, int level){
+    public BoardView(JFrame frame, int level) {
         this.frame = frame;
         this.level = level;
         setName("Board");
@@ -33,34 +32,91 @@ public class BoardView extends JPanel {
 
         add(goBackBtn, BorderLayout.PAGE_START);
         launch();
-
-        //createPngTiles();
     }
 
-    private void createPngTiles(){
+    private HashMap<String, BufferedImage> createGraySquareTiles(){
         BufferedImage gridImage = null;
+        HashMap<String, BufferedImage> grayImages= new HashMap<>();
         try {
-            gridImage = ImageIO.read(new File("src/res/carte_tuiles.png"));
+            gridImage = ImageIO.read(new File("src/res/tuiles.png"));
+            BufferedImage square = gridImage.getSubimage(0, 0, 120, 120);
+            BufferedImage wifi = gridImage.getSubimage(120, 120, 120, 120);
+            BufferedImage lamp = gridImage.getSubimage(240, 120, 120, 120);
+            BufferedImage connection = gridImage.getSubimage(0 ,240, 120 , 120);
+            BufferedImage curve = gridImage.getSubimage(120 ,240, 120 , 120);
+            BufferedImage line = gridImage.getSubimage(240 ,240, 120 , 120);
+            BufferedImage source = gridImage.getSubimage(0, 480, 120, 120);
 
-        // Calculate the width and height of each small image
-        int smallWidth = gridImage.getWidth() / 8;
-        int smallHeight = gridImage.getHeight() / 6;
+            grayImages.put("square", square);
+            grayImages.put("wifi", wifi);
+            grayImages.put("lamp", lamp);
+            grayImages.put("connection", connection);
+            grayImages.put("curve", curve);
+            grayImages.put("line", line) ;
+            grayImages.put("source", source) ;
 
-        // Crop the grid image into 9 small images
-        int count = 1;
-        for (int y = 0; y < 8; y++) {
-            for (int x = 0; x < 6; x++) {
-                BufferedImage smallImage = new BufferedImage(smallWidth, smallHeight, gridImage.getType());
-                smallImage.getGraphics().drawImage(gridImage, 0, 0, smallWidth, smallHeight, smallWidth*x, smallHeight*y, smallWidth*(x+1), smallHeight*(y+1), null);
-                ImageIO.write(smallImage, "png", new File("small" + count + ".png"));
-                count++;
-            }
-        }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return grayImages;
     }
 
+    private HashMap<String, BufferedImage> createGrayHexagoneTiles(){
+        BufferedImage gridImage ;
+        HashMap<String, BufferedImage> grayImages= new HashMap<>();
+        try {
+            gridImage = ImageIO.read(new File("src/res/tuiles.png"));
+            BufferedImage hexagone = gridImage.getSubimage(360, 0, 120, 104);
+            BufferedImage wifi = gridImage.getSubimage(480, 120, 120, 104);
+            BufferedImage lamp = gridImage.getSubimage(600, 120, 120, 104);
+
+            BufferedImage connection = gridImage.getSubimage(360 ,240, 120 , 104);
+            BufferedImage curve = gridImage.getSubimage(480 ,240, 120 , 104);
+            BufferedImage large_curve = gridImage.getSubimage(600 ,240, 120 , 104);
+            BufferedImage line = gridImage.getSubimage(720 ,240, 120 , 104);
+            BufferedImage source = gridImage.getSubimage(480, 480, 120, 104);
+
+            grayImages.put("hexagone", hexagone);
+            grayImages.put("wifi", wifi);
+            grayImages.put("lamp", lamp);
+            grayImages.put("connection", connection);
+            grayImages.put("curve", curve);
+            grayImages.put("large_curve", large_curve);
+            grayImages.put("line", line) ;
+            grayImages.put("source", source) ;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return grayImages;
+    }
+
+    private HashMap<String, BufferedImage> createWhiteSquareTiles(){
+        BufferedImage gridImage = null;
+        HashMap<String, BufferedImage> grayImages= new HashMap<>();
+        try {
+            gridImage = ImageIO.read(new File("src/res/tuiles.png"));
+            BufferedImage square = gridImage.getSubimage(0, 360, 120, 120);
+            BufferedImage wifi = gridImage.getSubimage(480, 120, 120, 120);
+            BufferedImage lamp = gridImage.getSubimage(480, 240, 120, 120);
+            BufferedImage source = gridImage.getSubimage(0, 480, 120, 120);
+            BufferedImage connection = gridImage.getSubimage(0 ,600, 120 , 120);
+            BufferedImage curve = gridImage.getSubimage(120 ,600, 120 , 120);
+            BufferedImage line = gridImage.getSubimage(240 ,600, 120 , 120);
+
+            grayImages.put("square", square);
+            grayImages.put("wifi", wifi);
+            grayImages.put("lamp", lamp);
+            grayImages.put("connection", connection);
+            grayImages.put("curve", curve);
+            grayImages.put("line", line) ;
+            grayImages.put("source", source) ;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return grayImages;
+    }
     private String createFileName(String shape, String role){
         return "src/res/"+shape+"_images/"+role+".png";
     }
@@ -198,41 +254,105 @@ public class BoardView extends JPanel {
                         case 4 -> {
                             System.out.println("case 4 ");
                             // assuming imageIcon contains the image you want to rotate
-
-
                         }
                         case 5->{
                             System.out.println("case 5 ");
                         }
-
                     }
                 }
             }
-
-
             panel.add(tilePanel);
         }
 
         return panel;
 
     }
+
+
     private void launch(){
         Level level = new Level(this.level);
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.darkGray);
+        panel.setSize(300,300);
+        GridLayout layout;
+        String shape = "";
+        HashMap<String, BufferedImage> grayTiles;
         if(level.getShape()== 'H'){
+            shape = "hexagone";
             board = new HexaBoard(level.getTileConfig(), level.getWidth(), level.getHeight());
-
-            add(createImages( "hexagone"));
+            layout= new GridLayout();
+            panel.setLayout(layout);
             System.out.println("size : "+board.getRows() + " "+  board.getColumns());
 
             System.out.println("hexa board \n" );
+            grayTiles = createGrayHexagoneTiles() ;
 
         }else{
+            shape = "square";
             board = new SquareBoard(level.getTileConfig(), level.getWidth(), level.getHeight());
-            System.out.println("size : "+board.getRows() + " "+  board.getColumns());
-            add(createImages( "square"));
-
-            System.out.println("square board \n" +  board.getBoard());
-
+            layout = new GridLayout(board.getRows(), board.getColumns());
+            grayTiles = createGraySquareTiles();
         }
+        panel.setLayout(layout);
+        for(Tile tile : board.getBoard()){
+            createSqaureTile(panel, grayTiles, tile, shape);
+        }
+        add(panel);
+        System.out.println("square board \n" +  board.getBoard());
+
+    }
+
+    private void createSqaureTile(JPanel panel, HashMap<String, BufferedImage> grayTiles, Tile tile, String shape){
+        JPanel tilePanel = new JPanel();
+        tilePanel.setLayout(new BorderLayout());
+        for(String label : grayTiles.keySet()){
+            ImageIcon borderIcon = new ImageIcon(grayTiles.get(shape));
+            // Create a JLabel to display the image
+            JLabel border = new JLabel(borderIcon);
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            gbc.weightx = 1.0;
+            gbc.weighty = 1.0;
+            border.setLayout(new GridBagLayout());
+            gbc.anchor = GridBagConstraints.CENTER;
+            System.out.println(tile.getRole().toString());
+            if(tile.getRole().toString().equalsIgnoreCase("Empty")){
+                // Load the image
+                ImageIcon roleIcon = new ImageIcon(grayTiles.get("curve"));
+                // Create a JLabel to display the image
+                JLabel role = new JLabel(roleIcon);
+                border.add(role, gbc);
+                tilePanel.add(border, BorderLayout.CENTER);
+            }
+            if(tile.getRole().toString().equalsIgnoreCase(label)){
+                ImageIcon roleIcon = new ImageIcon(grayTiles.get(label));
+                JLabel role = new JLabel(roleIcon);
+                border.add(role, gbc);
+                for(int edge : tile.getEdges()){
+                    System.out.println(edge);
+                    switch (edge) {
+                        case 0 -> gbc.anchor = GridBagConstraints.PAGE_START;
+                        case 1 -> gbc.anchor = GridBagConstraints.LINE_END;
+                        case 2 -> gbc.anchor = GridBagConstraints.PAGE_END;
+                        case 3 -> gbc.anchor = GridBagConstraints.LINE_START;
+                        case 4 -> System.out.println("case4");
+                        case 5 -> System.out.println("case5");
+                    }
+                    border.add(addConnection(grayTiles.get("connection")), gbc);
+                }
+                tilePanel.add(border, BorderLayout.CENTER);
+            }
+        }
+        panel.add(tilePanel);
+    }
+
+    private JLabel addConnection(BufferedImage connection){
+
+        ImageIcon edgeIcon= new ImageIcon(connection);
+        /* Image scaledConnectionImage = edgeIcon.getImage().getScaledInstance(
+                            edgeIcon.getIconWidth() / 2, edgeIcon.getIconHeight() / 2, Image.SCALE_SMOOTH);
+        edgeIcon = new ImageIcon(scaledConnectionImage);*/
+        return new JLabel(edgeIcon);
     }
 }
