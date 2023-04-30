ced24696fd5fd4a683a2341cc4909ca4359c60e7
rmartini <reen.martini7@etu.univ-lorraine.fr>
2023-04-30 15:57:12 +0200

    adding untracked files

---
+++ src/controllers/BoardController.java
@@ -0,0 +1,40 @@
+package controllers;
+import models.Board;
+import models.Level;
+import views.BoardView;
+import views.TileView;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class BoardController {
+    private BoardView view;
+    private Board board;
+    private Level level;
+
+    public BoardController(JFrame frame, int l) {
+        this.level = new Level(l);
+        this.view = new BoardView(frame, l);
+        this.board = new Board(level.getHeight(), level.getWidth(), level.getTileConfig(), level.getShape() == 'S');
+        view.setBoard(board);
+        view.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                handleTileClick(e);
+            }
+        });
+    }
+
+    private void handleTileClick(MouseEvent e) {
+        Point clickPosition = e.getPoint();
+        for (TileView tileView : view.getTileViews()) {
+            Point tilePosition = tileView.getPosition();
+            if (view.contains(tilePosition, clickPosition)) {
+                tileView.setImage(view.rotateImage(tileView.getImage(), 90));
+                view.repaint();
+            }
+        }
+    }
+}
